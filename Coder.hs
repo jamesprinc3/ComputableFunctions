7db9a9f85f770e244f8a8f8724c79e6f394dd214
James Prince <Prince@Jamess-MBP-2.home>
2015-12-13 22:09:34 +0000

    fixed a bug Credit: James Long

--- Coder.hs
+++ Coder.hs
@@ -2,7 +2,7 @@ module Coder where
 
 import Defs
 
-toSinglePair :: Int -> Pair
+toSinglePair :: Integer -> Pair
 toSinglePair = toSinglePair' . toString . decToRevBin
 
 toSinglePair' :: String -> Pair
@@ -10,11 +10,11 @@ toSinglePair' str = SB (I x) (I y)
   where
     strlen = (length str)
     xstrip = dropWhile ((==) '1') str
-    x      = strlen - (length xstrip)
-    y      = binToDec . reverse . tail $ xstrip
+    x      = toInteger (strlen - (length xstrip))
+    y      = toInteger (binToDec . reverse . tail $ xstrip)
 
 
-toDoublePair :: Int -> Pair
+toDoublePair :: Integer -> Pair
 toDoublePair = toDoublePair' . toString . decToRevBin
 
 toDoublePair' :: String -> Pair
@@ -22,24 +22,24 @@ toDoublePair' str = DB (I x) (I y)
   where
     strlen = (length str)
     xstrip = dropWhile ((==) '0') str
-    x      = strlen - (length xstrip)
-    y      = binToDec . reverse . tail $ xstrip
+    x      = toInteger (strlen - (length xstrip))
+    y      = toInteger (binToDec . reverse . tail $ xstrip)
 
 
-pairToInt :: Pair -> Int
+pairToInt :: Pair -> Integer
 pairToInt (I i)    = i
 pairToInt (DB x y) = (2^(pairToInt x))*(2*(pairToInt y)+1)
 pairToInt (SB x y) = (2^(pairToInt x))*(2*(pairToInt y)+1) - 1
 
 
-listToInt :: List -> Int
+listToInt :: List -> Integer
 listToInt [] = 0
 listToInt ((Ad x):xs) = (2^x)*(2*y+1)
   where
     y = listToInt xs
 
 
-intToList :: Int -> List
+intToList :: Integer -> List
 intToList i = intToList' x
   where
     Bin x = decToRevBin i
@@ -49,12 +49,12 @@ intToList' :: String -> List
 intToList' []  = []
 intToList' str = (Ad x):xs
   where
-    x = length (takeWhile ((==) '0') str)
+    x  = toInteger (length (takeWhile ((==) '0') str))
     xs = intToList' (drop 1 (dropWhile ((==) '0') str))
 
--- data Instr = HALT | RPLUS Int Label | RMINUS Int Label Label (for ref)
+-- data Instr = HALT | RPLUS Integer Label | RMINUS Integer Label Label (for ref)
 
-progToInt :: Program -> Int
+progToInt :: Program -> Integer
 progToInt ps = (listToInt (progToInt' ps))
 
 progToInt' :: Program -> List
@@ -70,15 +70,15 @@ progToInt' ((l, RMINUS i (L j) (L k)):ls) = (Ad body):(progToInt' ls)
 -- L0: R0 L0 L2
 -- L1: HALT
 
-intToProg :: Int -> Program
+intToProg :: Integer -> Program
 --intToProg i = zip (repeat (L 0)) (map (intToInstr . toInt) xs)
-intToProg i = zip [(L n) | n <- [0..leng]] (map (intToInstr . toInt) xs)
+intToProg i = zip [(L (toInteger n)) | n <- [0..leng]] (map (intToInstr . toInt) xs)
   where
     xs   = intToList i
     leng = length xs
 
 
-intToInstr :: Int -> Instr
+intToInstr :: Integer -> Instr
 intToInstr 0 = HALT
 intToInstr x
   |y `mod` 2 == 0 = RPLUS i (L z)
@@ -93,14 +93,12 @@ intToInstr x
       k      = pairToInt d 
 
 
-instrToInt :: Instr -> Int
+instrToInt :: Instr -> Integer
 instrToInt HALT                   = 0
-instrToInt (RPLUS i (L j))        = pairToInt (DB (I i) (I j))
-instrToInt (RMINUS i (L j) (L k)) = pairToInt (DB (I i) (SB (I j) (I k)))
+instrToInt (RPLUS i (L j))        = pairToInt (DB (I (2*i)) (I j))
+instrToInt (RMINUS i (L j) (L k)) = pairToInt (DB (I ((2*i)+1)) (SB (I j) (I k)))
 
 
--- Answer to 1c given by: map (instrToInt . snd)  p
--- [46,0,286,1150,0,1]
 
 
 
--- Defs.hs
+++ Defs.hs
@@ -3,20 +3,20 @@ module Defs where
 import Data.Char
 import Data.List
 
-data Pair = I Int
-          | SB Pair Pair   -- <Int, Int>
-          | DB Pair Pair   -- <<Int, Int>>
+data Pair = I Integer
+          | SB Pair Pair   -- <Int, Integer>
+          | DB Pair Pair   -- <<Int, Integer>>
 
 instance Show Pair where 
   show (SB a b) = "<" ++ (show a) ++ "," ++ (show b) ++ ">"
   show (DB a b) = "<<" ++ (show a) ++ "," ++ (show b) ++ ">>"
 
-data Label = L Int
+data Label = L Integer
 
 instance Show Label where
   show (L a) = "L" ++ (show a)
 
-data Instr = HALT | RPLUS Int Label | RMINUS Int Label Label
+data Instr = HALT | RPLUS Integer Label | RMINUS Integer Label Label
 
 instance Show Instr where
   show HALT = "HALT"
@@ -33,9 +33,9 @@ newtype Binary = Bin {toString :: String}
 instance Show Binary where
   show (Bin s) = "0b" ++ s
 
---data Addr = Ad Int
+--data Addr = Ad Integer
 
-newtype Addr = Ad {toInt :: Int}
+newtype Addr = Ad {toInt :: Integer}
 
 instance Show Addr where 
   show (Ad a) = (show a)
@@ -47,20 +47,20 @@ type Program = [Line]
 -- <<x, y>> = 2^x(2y+1)
 -- <x, y>   = 2^x(2y+1) - 1
 
-decToRevBin :: Int -> Binary
+decToRevBin :: Integer -> Binary
 decToRevBin x = Bin (decToRevBin' x)
 
-decToRevBin' :: Int -> String
+decToRevBin' :: Integer -> String
 decToRevBin' 0 = []
-decToRevBin' y = (intToDigit b):(decToRevBin' a)
+decToRevBin' y = ((intToDigit . fromInteger) b):(decToRevBin' a)
   where 
     (a,b) = quotRem y 2
 
-binToDec :: String -> Int
-binToDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+binToDec :: String -> Integer
+binToDec = foldl' (\acc x -> acc * 2 + (toInteger . digitToInt) x) 0
 
 
--- for Q1b
+-- for Q1c
 p = [(L 0, RMINUS 1 (L 2) (L 1)),
      (L 1, HALT),
      (L 2, RMINUS 1 (L 3) (L 4)),
@@ -68,4 +68,12 @@ p = [(L 0, RMINUS 1 (L 2) (L 1)),
      (L 4, HALT),
      (L 5, RPLUS 0 (L 0))
     ]
+-- Answer to Q1c given by: map (instrToInt . snd)  p
+-- [46,0,286,1150,0,1]
+
+-- for Q2
+n :: Integer
+n = (2^46) * 20483
+-- Answer for Q2 given by intToProg n 
+-- [(L0,R0-, L2, L1),(L1,HALT),(L2,R0-, L0, L1),(L3,R0+, L0)]
 
